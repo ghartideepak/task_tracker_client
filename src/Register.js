7bebca9ef4554f6a2f361de7da21cd7c38fae9de
ghartideepak <149758743+ghartideepak@users.noreply.github.com>
2025-06-29 21:55:41 -0400

    Update Register.js

--- src/Register.js
+++ src/Register.js
@@ -12,7 +12,8 @@ function Register({ onRegister }) {
     setSuccess('');
 
     try {
-      const res = await fetch('http://localhost:5000/api/auth/register', {
+      const res = await fetch('https://task-tracker-0afe.onrender.com
+/api/auth/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
